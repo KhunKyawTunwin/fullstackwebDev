89865b62410abdf0313bea2598a1fc1b91d763df
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-01-31 00:01:56 +0630

    New Updated Libary

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-
+const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
 
 app.set("view engine", "ejs");
@@ -10,8 +14,14 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 
-app.use("/", indexRouter);
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 
+app.use("/", indexRouter);
 app.listen(process.env.PORT || 5000, () =>
   console.log("Server running at http://localhost:5000")
 );
