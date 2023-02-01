e375252ac3897bf48130327be910cc7e412017e6
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-02-01 11:03:17 +0630

    Env Updated

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-// if (process.env.NODE_ENV !== "production") {
-//   require("dotenv").config();
-// }
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/author");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -22,6 +23,9 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+
 app.listen(process.env.PORT || 5000, () =>
   console.log("Server running at http://localhost:5000")
 );
+// DATABASE_URL = "mongodb://127.0.0.1:27017/webdev"
